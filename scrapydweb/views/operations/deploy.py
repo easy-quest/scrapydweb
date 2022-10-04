f184aecba58786cab7010b4f538676328386bdf8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 04:48:23 +0000

    Adding numeric_owner as keyword arguement

--- scrapydweb/views/operations/deploy.py
+++ scrapydweb/views/operations/deploy.py
@@ -394,7 +394,7 @@ class DeployUploadView(BaseView):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar, tmpdir)
